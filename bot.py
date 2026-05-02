c45e81862fce1de79ad52235f9bb3e60e67f17bc
Afrsto <afratosa@gmail.com>
2026-05-02 22:01:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,10 +17,6 @@ from github import Github, GithubException
 
 from netflix_checker import check_cookie_file
 
-# ╔══════════════════════════════════════════════════════════════╗
-# ║                      CONFIGURATION                          ║
-# ╚══════════════════════════════════════════════════════════════╝
-
 COOKIES_FOLDER         = Path("cookies")
 SCRIPT_TIMEOUT         = 30
 CONFIG_FILE            = Path("config.json")
